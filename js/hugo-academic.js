d0daac25b55433d87b453100c3a9705cb0b182d6
stes <steffen.schneider@rwth-aachen.de>
2017-04-04 22:58:42 +0100

    Updated page

--- js/hugo-academic.js
+++ js/hugo-academic.js
@@ -116,7 +116,7 @@
   $(window).load(function(){
 
     // Enable smooth scrolling with mouse wheel
-    smoothScroll(1.3, 220);
+    // smoothScroll(1.3, 220);
 
     // When accessing homepage from another page and `#top` hash is set, show top of page (no hash).
     if (window.location.hash == "#top") {
